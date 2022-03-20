a6799d446780209c4cd1f1e4389ac79c886de2df
Anastasia Usoltseva <neeeerk@yandex.ru>
2022-03-20 22:04:14 +0300

    start

---
+++ src/main/java/ru/mycomp/preproj231/Preproj231Application.java
@@ -0,0 +1,13 @@
+package ru.mycomp.preproj231;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Preproj231Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Preproj231Application.class, args);
+	}
+
+}
